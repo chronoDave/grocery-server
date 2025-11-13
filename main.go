e9d490fe421e20a729fec1361535b9c8428732ed
chronoDave <davidwolters6758@gmail.com>
2025-11-13 15:05:07 +0000

    Added authentication

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -11,6 +13,11 @@ import (
 type Item struct {
 	Name   string `json:"name"`
 	Amount int    `json:"amount"`
+}
+
+type User struct {
+	Username string
+	Password string
 }
 
 func read(path string) []Item {
@@ -44,11 +51,51 @@ func write(path string, list []Item) {
 
 const PORT = 3333
 
+func auth(user User, next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		username, password, ok := request.BasicAuth()
+
+		if ok {
+			username_actual := sha256.Sum256([]byte(username))
+			username_expected := sha256.Sum256([]byte(user.Username))
+			password_actual := sha256.Sum256([]byte(password))
+			password_expected := sha256.Sum256([]byte(user.Password))
+
+			username_match := subtle.ConstantTimeCompare(username_actual[:], username_expected[:]) == 1
+			password_match := subtle.ConstantTimeCompare(password_actual[:], password_expected[:]) == 1
+
+			if username_match && password_match {
+				next.ServeHTTP(writer, request)
+
+				return
+			}
+		}
+
+		writer.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+		http.Error(writer, "Unauthorized", http.StatusUnauthorized)
+	})
+}
+
+func env() User {
+	file, err := os.ReadFile("env.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var user User
+	if err := json.Unmarshal(file, &user); err != nil {
+		panic(err)
+	}
+
+	return user
+}
+
 func main() {
+	user := env()
 	list := read("grocery.json")
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /", func(writer http.ResponseWriter, request *http.Request) {
+	mux.HandleFunc("GET /", auth(user, func(writer http.ResponseWriter, request *http.Request) {
 		payload, err := json.Marshal(list)
 		if err != nil {
 			http.Error(writer, err.Error(), http.StatusInternalServerError)
@@ -57,9 +104,9 @@ func main() {
 
 		writer.Header().Set("Content-Type", "application/json")
 		writer.Write(payload)
-	})
+	}))
 
-	mux.HandleFunc("POST /", func(writer http.ResponseWriter, request *http.Request) {
+	mux.HandleFunc("POST /", auth(user, func(writer http.ResponseWriter, request *http.Request) {
 		decoder := json.NewDecoder(request.Body)
 		decoder.DisallowUnknownFields()
 
@@ -80,7 +127,7 @@ func main() {
 
 		writer.Header().Set("Content-Type", "application/json")
 		writer.Write(payload)
-	})
+	}))
 
 	fmt.Printf("Starting server on port: %d\n", PORT)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), mux)
